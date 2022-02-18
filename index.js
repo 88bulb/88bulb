4cdc1a04420a42110fbea9ccdf0c30d1b0961508
matianfu <matianfu@gingerologist.com>
2022-02-18 20:03:26 +0800

    update scripts

--- index.js
+++ index.js
@@ -1,32 +1,63 @@
 const fs = require('fs')
 const net = require('net')
 
-fs.readFile('assets/bulbcast-0024.bin', (err, buf) => {
-  if (err) return
-  const size = Buffer.alloc(4)
-  size.writeUInt32LE(buf.length)
-  const hash = buf.slice(buf.length - 32).toString('hex')
-  console.log(`sha256: ${hash}`)
-  console.log(`sha80: ${hash.slice(0, 20)}`)
-  console.log(`b01bb007a0764e689fb6${hash.slice(0, 20)}a5a5a5a50200`)
-  const server = net.createServer(socket =>
-    socket.on('data', data => {
-      const text = data.toString()
-      console.log(text)
-      if (text[text.length - 1] === '\n') {
-        const kv = text.trim().split(' ')
-        if (kv.length === 2 &&
-            kv[0] === 'GET' &&
-            kv[1].length === 20 &&
-            hash.startsWith(kv[1])) {
-	  console.log('right kv')
-          socket.write(Buffer.concat([size, buf]))
-        } else {
-          console.log('wrong kv')
-	}
-      }
-      socket.end()
-    })
-  ).listen({ host: '10.42.0.1', port: 6016 },
-    () => console.log(server.address()))
+// const filename = fs.readdirSync('assets')
+//   .filter(x => x.match('^bulbcast-[0-9][0-9][0-9][0-9].bin$'))
+//   .sort()
+//   .pop()
+//
+// console.log(`latest bin: ${filename}`)
+//
+
+fs.readdir('assets', (err, files) => {
+  if (err || files.length === 0) process.exit(1)
+
+  const filename =
+      files.filter(x => x.match('^bulbcast-[0-9][0-9][0-9][0-9].bin$'))
+        .sort()
+        .pop()
+
+  console.log(`latest bin: ${filename}`)
+
+  fs.readFile(`assets/${filename}`, (err, buf) => {
+    if (err) { return }
+    const size = Buffer.alloc(4)
+    size.writeUInt32LE(buf.length)
+    const hash = buf.slice(buf.length - 32).toString('hex')
+    console.log(`sha256: ${hash}`)
+    console.log(`sha80: ${hash.slice(0, 20)}`)
+    console.log(`b01bb007a0764e689fb6${hash.slice(0, 20)}a5a5a5a50200`)
+    console.log(`b01bb007a0764e655e8e${hash.slice(0, 20)}a5a5a5a50200`)
+    console.log(`b01bb007a0764e653ba6${hash.slice(0, 20)}a5a5a5a50200`)
+    console.log(`b01bb007a0764e655eb2${hash.slice(0, 20)}a5a5a5a50200`)
+    console.log(`b01bb007a0764e655f26${hash.slice(0, 20)}a5a5a5a50200`)
+    console.log(`b01bb007a0764e655f3e${hash.slice(0, 20)}a5a5a5a50200`)
+    console.log(`b01bb007a0764e655ec6${hash.slice(0, 20)}a5a5a5a50200`)
+    console.log(`b01bb007a0764e689ffe${hash.slice(0, 20)}a5a5a5a50200`)
+    console.log(`b01bb007a0764e655e92${hash.slice(0, 20)}a5a5a5a50200`)
+    console.log('\n')
+
+
+    const server =
+          net.createServer(socket => socket.on(
+            'data',
+            data => {
+              const text = data.toString()
+              console.log(text)
+              if (text[text.length - 1] === '\n') {
+                const kv = text.trim().split(' ')
+                if (kv.length === 2 && kv[0] === 'GET' &&
+                                       kv[1].length === 20 &&
+                                       hash.startsWith(kv[1])) {
+                  console.log('right kv')
+                  socket.write(Buffer.concat([size, buf]))
+                } else {
+                  console.log('wrong kv')
+                }
+              }
+              socket.end()
+            }))
+            .listen({ host: '10.42.0.1', port: 6016 },
+              () => console.log(server.address()))
+  })
 })
--- screen.js
+++ screen.js
@@ -2,6 +2,55 @@ const fs = require('fs')
 const net = require('net')
 const blessed = require('blessed')
 const SerialPort = require('serialport')
+const strip = require('strip-color')
+
+const bulbs = []
+
+const hexChar =
+        [
+          '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
+          'e', 'f'
+        ]
+
+const cycleColor = async () => {
+  let v = 0
+  while (1) {
+    const b = (v % 2 === 0) ? 4 : 1
+    const h = v % 256
+    const seq = v % 256
+    const seqHex = hexChar[Math.floor(seq / 16)] + hexChar[seq % 16]
+
+    const cmd = `b01bc0de${seqHex}a5a5a5a50001` +
+      `000280${hexChar[Math.floor(h / 16)]}${hexChar[h % 16]}ff400002` + '00000000000000'
+
+    port.write(cmd + '\n', 'ascii', () => log(cmd))
+
+    v++
+
+    await new Promise((res, rej) => setTimeout(() => res(null), 220))
+  }
+}
+
+const tripleColor = async () => {
+  let v = 0
+  const colors =
+      [
+        '008040',
+        '808040',
+      ]
+
+// b01bc0de02a5a5a5a50001100200004010ff6000000000000000
+  while (1) {
+    const seq = v % 256
+    const seqHex = hexChar[Math.floor(seq / 16)] + hexChar[seq % 16]
+    const color = colors[v % colors.length]
+    //               b01bc0de00       a5a5a5a500011002000020 88ff33 00000000000000
+    const cmdline = `b01bc0de${seqHex}a5a5a5a500031002000005${color}00000000000000\n`
+    port.write(cmdline, 'ascii', () => log(cmdline.trim()))
+    v++
+    await new Promise((res, rej) => setTimeout(() => res(null), 600))
+  }
+}
 
 const args = process.argv.slice(2)
 const idx = args.indexOf('-p')
@@ -17,7 +66,18 @@ port.on('error', (err) => {
 })
 
 port.on('data', data => {
-//  console.log('data: ', data.toString().trim())
+  const line = strip(data.toString().trim())
+  // log('serial: ' + line)
+
+  const segments = line.split(' ')
+  if (segments.length === 3 && segments[0] === 'bulbcast') {
+    if (!bulbs.includes(segments[1])) {
+      log('new device found: ' + segments[1])
+      bulbs.push(segments[1])
+    }
+
+    log(line)
+  }
 })
 
 const screen = blessed.screen()
@@ -27,7 +87,7 @@ const body = blessed.box({
   height: '100%-1',
   width: '100%',
   keys: false,
-  mouse: false,
+  mouse: true,
   alwaysScroll: true,
   scrollable: true,
   scrollbar: { ch: ' ', bg: 'red' }
@@ -57,6 +117,10 @@ inputBar.on('submit', (text) => {
     process.exit(0)
   } else if (text === 'clear') {
     body.setContent('')
+  } else if (text.startsWith('cycle color')) {
+    cycleColor().then(() => {}).catch(err => {})
+  } else if (text.startsWith('tricolor')) {
+    tripleColor().then(() => {}).catch(err => {})
   } else {
     log(text)
   }
